cb5f89413e5781fd4d25b5c6c0a870e484d56555
khushi <nagar.khushi2001@gmail.com>
2023-05-24 16:58:24 +0530

    first commit

---
+++ src/Components/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+// import { BrowserRouter as } from 'react-router-dom'
+import ".../Components/Styles/App.scss"
+export const App = () => {
+  return (
+    <div>App</div>
+  )
+}
